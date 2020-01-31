9a971bd43a8a4326ce7726f66830af90320d49fa
Krister Torikka <krister.torikka@gmail.com>
2020-01-31 22:53:01 +0200

    Reformat code and improve styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,70 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import clsx from 'clsx';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import styled from "styled-components";
 
 const Navbar = ({ classes, open, handleDrawerOpen }) => {
- return (
+  return (
     <AppBar
-    position="fixed"
-    className={clsx(classes.appBar, {
-      [classes.appBarShift]: open,
-    })}
-  >
-    <Toolbar>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={handleDrawerOpen}
-        edge="start"
-        className={clsx(classes.menuButton, open && classes.hide)}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Link to="/">
-        <Typography variant="h6" noWrap>
-          Webshop
-        </Typography>
-      </Link>
-      <Link to='/cart' className="ml-auto">
-        <StyledCartButton>
-            <ShoppingCartIcon/>
+      style={{ background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)" }}
+      position="fixed"
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: open
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          className={clsx(classes.menuButton, open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Link to="/" style={{ color: "#FFF" }}>
+          <Typography variant="h6" noWrap>
+            Webshop
+          </Typography>
+        </Link>
+        <Link to="/cart" className="ml-auto">
+          <StyledCartButton>
+            <ShoppingCartIcon />
             Cart
-        </StyledCartButton>
-      </Link>
-    </Toolbar>
-  </AppBar>
- )
-}
+          </StyledCartButton>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 const StyledMenuButton = styled.button`
-    background: transparent;
-    color: white;
-    display: inline-block;
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid white;
-    border-radius: 5px;
-    display: block;
-`
+  background: transparent;
+  color: white;
+  display: inline-block;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid white;
+  border-radius: 5px;
+  display: block;
+`;
 
 const StyledCartButton = styled.button`
-    background: rgb(55, 185, 51);
-    color: white;
-    display: inline-block;
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid rgb(55, 185, 51);
-    border-radius: 5px;
-    display: block;
-`
+  background: rgb(55, 185, 51);
+  color: white;
+  display: inline-block;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid rgb(55, 185, 51);
+  border-radius: 5px;
+  display: block;
+`;
 
 export default Navbar;
