3aa20413a4acd6a480769e88b39a4cbf4ef58de3
Krister Torikka <krister.torikka@gmail.com>
2020-01-31 22:53:36 +0200

    Add mock data

---
+++ src/mockData.jsx
@@ -0,0 +1,57 @@
+export const mockProducts = [
+  {
+    id: 1,
+    title: "Iphone 7",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 399.99,
+    img: "images/iphone7.png",
+    company: "Apple",
+    inCart: false,
+    count: 0
+  },
+  {
+    id: 2,
+    title: "Macbook Pro",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 999.99,
+    img: "images/macbook_pro.png",
+    company: "Apple",
+    inCart: false,
+    count: 0
+  },
+  {
+    id: 3,
+    title: "Dell Laptop",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 349.99,
+    img: "images/dell_laptop.png",
+    company: "Dell",
+    inCart: false,
+    count: 0
+  },
+  {
+    id: 4,
+    title: "iMac",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 1299.99,
+    img: "images/imac.png",
+    company: "Apple",
+    inCart: false,
+    count: 0
+  },
+  {
+    id: 5,
+    title: "Xbox One",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 399.99,
+    img: "images/xbox_one.png",
+    company: "Microsoft",
+    inCart: false,
+    count: 0
+  }
+];
