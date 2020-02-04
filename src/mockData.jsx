a6eae073f1a6f34482081568a781f3b6dd468b6d
Krister Torikka <krister.torikka@gmail.com>
2020-02-04 22:14:08 +0200

    Add more mock data

--- src/mockData.jsx
+++ src/mockData.jsx
@@ -53,5 +53,27 @@ export const mockProducts = [
     company: "Microsoft",
     inCart: false,
     count: 0
+  },
+  {
+    id: 6,
+    title: "Playstation 4",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 399.99,
+    img: "images/playstation4.png",
+    company: "Sony",
+    inCart: false,
+    count: 0
+  },
+  {
+    id: 7,
+    title: "Nintendo Switch",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 299.99,
+    img: "images/nintendo_switch.png",
+    company: "Nintendo",
+    inCart: false,
+    count: 0
   }
 ];
